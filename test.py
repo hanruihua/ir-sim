0b0f5fcb09effb02c222e186e40e7e76c7152693
Han <744475118@qq.com>
2022-06-06 21:00:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-import gym
+class Bird:
+    #鸟有翅膀
+    def isWing(self):
+        print("鸟有翅膀")
+    #鸟会飞
+    def fly(self):
+        print("鸟会飞")
+class Ostrich(Bird):
+    # 重写Bird类的fly()方法
+    def fly(self, a):
+        print("鸵鸟不会飞")
+        print(a)
+# 创建Ostrich对象
+ostrich = Ostrich()
+#调用 Bird 类中的 fly() 方法
+ostrich.fly(1)
