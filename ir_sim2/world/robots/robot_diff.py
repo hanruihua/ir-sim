4bb636b1142a81bd7c130a364777b4fda880fb63
Han <744475118@qq.com>
2022-06-07 21:42:58 +0800

    Update robot_diff.py

--- ir_sim2/world/robots/robot_diff.py
+++ ir_sim2/world/robots/robot_diff.py
@@ -2,9 +2,11 @@ import robot_base
 import numpy as np
 
 class robot_diff(robot_base):
-    def __init__(self, id, shape='circle', step_time=0.1, radius=0.2, radius_exp=0.1, **kwargs):
-        super(robot_diff, self).__init__(id=id, shape=shape, robot_type='diff', step_time=step_time, **kwargs)
 
+    robot_type = 'diff'
+    
+    def __init__(self, id, shape='circle', step_time=0.1, radius=0.2, radius_exp=0.1, **kwargs):
+        super(robot_diff, self).__init__(id=id, shape=shape, robot_type=robot_diff.robot_type, step_time=step_time, **kwargs)
         self.radius = radius
         self.radius_collision = radius + radius_exp
         self.vel_omni = np.zeros(self.vel_dim)
@@ -15,32 +17,32 @@ class robot_diff(robot_base):
         # vel_tpe: 'diff' or 'omni'
         if vel_type == 'omni':
             pass
-        
-        self.state = self.motion_diff(self.state, vel, step_time, noise, alpha)
-        self.vel = vel
+            self.vel_omni = vel
+
+        elif vel_type == 'diff':
+            self.state = robot_diff.motion_diff(self.state, vel, step_time, noise, alpha)
+            self.vel = vel
         # self.vel_omni = 
 
+    def gen_inequal(self,):
+        pass
+
     # reference: probabilistic robotics[book], motion model P127
-    def motion_diff(self, current_state, vel, sampletime, noise = False, alpha = [0.01, 0, 0, 0.01, 0, 0]):
+    @classmethod
+    def motion_diff(cls, current_state, vel, sampletime, noise = False, alpha = [0.01, 0, 0, 0.01, 0, 0]):
+        
+        assert current_state.shape == self.state_dim and vel.shape == self.vel_dim and cls.robot_type == 'diff'
 
-        # vel: np.array([[linear vel],[angular vel]])
-        # current_state: np.array([[x], [y], [theta]])
-        # alpha: control noise includes linear, angular, orientation
-
-        if noise == True:
-
+        if noise:
             std_linear = np.sqrt(alpha[0] * (vel[0, 0] ** 2) + alpha[1] * (vel[1, 0] ** 2))
             std_angular = np.sqrt(alpha[2] * (vel[0, 0] ** 2) + alpha[3] * (vel[1, 0] ** 2))
             # gamma = alpha[4] * (vel[0, 0] ** 2) + alpha[5] * (vel[1, 0] ** 2)
-            
-            vel_noise = vel + np.random.normal([[0], [0]], scale = [[std_linear], [std_angular]])
-
+            real_vel = vel + np.random.normal([[0], [0]], scale = [[std_linear], [std_angular]])
         else:
-            vel_noise = vel
+            real_vel = vel
 
-        vt = float(vel_noise[0, 0])
-        omegat = float(vel_noise[1,0])
-
+        vt = float(real_vel[0, 0])
+        omegat = float(real_vel[1, 0])
         theta = float(wraptopi(current_state[2, 0]))
 
         if omegat >= 0.01 or omegat <= -0.01:
@@ -48,7 +50,6 @@ class robot_diff(robot_base):
             next_state = current_state + np.array([ [-ratio * sin(theta) + ratio * sin(theta + omegat * sampletime)], 
                                         [ratio * cos(theta) - ratio * cos(theta + omegat * sampletime)], 
                                         [omegat * sampletime]])
-
         else:
             next_state = current_state + np.array([[vt * sampletime * cos(theta)], [vt * sampletime * sin(theta)], [0]])
 
@@ -56,11 +57,18 @@ class robot_diff(robot_base):
         
         return next_state 
 
-    def diff_to_omni(self,):
+    @staticmethod
+    def diff_to_omni():
+        pass
+    
+    @staticmethod
+    def omni_to_diff():
+        pass
+    
+    @staticmethod
+    def wraptopi():
         pass
 
-    def omni_to_diff(self,):
-        pass
 
 
         
