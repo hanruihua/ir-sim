88d6fb207d4bc947c0d311bf83352d79839bd755
Herry_robot <744475118@qq.com>
2022-06-12 22:48:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
 import numpy as np
 
-a = np.c_[[100, 100]]
-amin = np.c_[[4, 10]]
-amax = np.c_[[5, 20]]
-# amin =
-
-# b = np.c_[a]
-print(a.shape)
-
-if (a < amin).any(): print('min')
-if (a > amax).any(): print('max')
-
-# min = np.array([[3], [3]])
-
-# max = np.array([[5], [7]])
-
-c = np.clip(a, amin, amax)
-
-print(c)
+a = {'1': 7, '2': 2, '1': 1}
+print(a.get('1', 10))
