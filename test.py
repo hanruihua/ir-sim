3c767a9102d225a192582817dc1bca30902b1b79
Han <744475118@qq.com>
2022-06-28 21:39:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,27 +4,7 @@ import random
 import numpy
 import time
 
-def worker(q):
-    plt.ion()
-    ln, = plt.plot([], [])
-    plt.show()
+a = [1, 2, 3]
+b, c, d = a
 
-    while True:
-        obj = q.get()
-        n = obj + 0
-        print("sub : got:"), n
-
-        ln.set_xdata(numpy.append(ln.get_xdata(), n))
-        ln.set_ydata(numpy.append(ln.get_ydata(), n))
-        plt.draw()
-
-if __name__ == '__main__':
-    queue = mp.Queue()
-    p = mp.Process(target=worker, args=(queue,))
-    p.start()
-
-    while True:
-        n = random.random() * 5
-        print("main: put:"), n
-        queue.put(n)
-        time.sleep(1.0)
+print(b, c)
