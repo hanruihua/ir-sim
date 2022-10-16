7b039ca3e4e7c6005520f422ebd2cb9cf2017629
Han <744475118@qq.com>
2022-10-16 19:46:26 +0800

    teleop

---
+++ ir_sim2/usage/11teleop_world/teleop_world.py
@@ -0,0 +1,20 @@
+from ir_sim2.env import EnvBase
+import time 
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = EnvBase(world_name = 'teleop_world.yaml', control_mode='keyboard')
+
+start_time = time.time()
+for i in range(3000):
+
+    env.step(env.key_vel)
+    env.render()
+
+    if env.done('any'): 
+        # env.reset()
+        break
+    
+    # env.reset(env.done_list(), 'any')  # 'all'; 'any'
+
+env.end(show_text=True)
