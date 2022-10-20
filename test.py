867cf6f33c9ccbaef16869e5b1770b0ef8d4f3da
Herry_robot <744475118@qq.com>
2022-10-20 21:52:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,11 @@
 import numpy as np
 import time
 import sys
+from ir_sim2 import env
+from ir_sim2.env import env_global
 
-print('2'.isdigit())
+# env_global.initialize()
+env_global.time_increment()
+
+print(env_global.time)
+
