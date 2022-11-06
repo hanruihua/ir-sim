8d115c2e9cce4e6b0092f01b03df94de3ea3c6f8
Herry_robot <744475118@qq.com>
2022-11-06 23:34:29 +0800

    add all world

---
+++ ir_sim2/usage/13All_things_world/all_world.py
@@ -0,0 +1,18 @@
+from ir_sim2.env import EnvBase, env_global
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+env = EnvBase(world_name = 'all_world.yaml', control_mode='keyboard')
+env = EnvBase(world_name = 'all_world.yaml', control_mode='keyboard')
+
+for i in range(3000):
+
+    env.step(env.key_vel, vel_id=env.key_id)
+
+    env.render(pause_time=0.001, show_traj=False, show_text=True)
+
+    if env.done():
+        break
+
+env.end(show_text=True)
