deb2730c19d7b6d04c872b305c2b2673fadc8061
Han <744475118@qq.com>
2023-02-27 16:51:38 +0800

    Add grid map

--- ir_sim/usage/21grid_map/grid_map.py
+++ ir_sim/usage/21grid_map/grid_map.py
@@ -1,7 +1,7 @@
 from ir_sim.env import EnvBase
 
-env = EnvBase('grid_map.yaml', control_mode='keyboard', init_args={'no_axis': False}, collision_mode='stop')
-# env = EnvBase('grid_map_car.yaml', control_mode='keyboard', init_args={'no_axis': False}, collision_mode='stop')
+env = EnvBase('grid_map.yaml', control_mode='keyboard', init_args={'no_axis': False}, collision_mode='stop', save_ani=False)
+# env = EnvBase('grid_map_car.yaml', control_mode='keyboard', init_args={'no_axis': False}, collision_mode='stop', save_ani=True)
 
 print(env.get_grid_map())
 
@@ -11,4 +11,4 @@ for i in range(300):
 
     if env.done(): break
         
-env.end()
+env.end(ani_name='grid_map', ani_kwargs={'subrectangles': True})
