855d54afe0d4f90f55a0b696eeb7c582602e5a8c
Han <744475118@qq.com>
2023-09-13 22:40:29 +0800

    Update

--- ir_sim/usage/12lidar_world/lidar_world.py
+++ ir_sim/usage/12lidar_world/lidar_world.py
@@ -1,13 +1,13 @@
 from ir_sim.env import EnvBase
 
-# env = EnvBase('lidar_world_car.yaml', control_mode='keyboard')
-env = EnvBase('lidar_world.yaml', control_mode='keyboard')
+env = EnvBase('lidar_world_car.yaml', control_mode='keyboard')
+# env = EnvBase('lidar_world.yaml', control_mode='keyboard')
 # env = EnvBase('lidar_world_omni.yaml', control_mode='keyboard')
 
 for i in range(3000):
 
     env.step()
-    env.render(show_text=True, show_goal=False)
+    env.render(show_text=False, show_goal=False, fig_kwargs={'bbox_inches': 'tight'})
     print(env.get_lidar_scan())
     env.reset('single')
 
