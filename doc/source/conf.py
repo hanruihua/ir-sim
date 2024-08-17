123acc5a69d8862cb0e3ba34bdd8854952e77889
Han <hanrh@connect.hku.hk>
2024-08-17 17:09:18 +0800

    build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('./ir_sim'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
