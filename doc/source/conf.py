3e36eb84131551d67317811689b401a0abd02b4a
Han <hanrh@connect.hku.hk>
2024-08-31 02:23:32 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,11 +25,11 @@ author = 'Ruihua Han'
 # The full version, including alpha/beta/rc tags
 release = '2.2.0'
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath(".."))
+# sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("../"))
+# sys.path.insert(0, os.path.abspath("../.."))
+# sys.path.insert(0, os.path.abspath('../../'))
+# sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ---------------------------------------------------
 
