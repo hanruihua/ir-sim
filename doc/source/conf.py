88faf92ee277c142bb4da8c3f5d6dbb9bc5a71d1
Han <hanrh@connect.hku.hk>
2024-08-31 02:33:12 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,9 @@ author = 'Ruihua Han'
 # The full version, including alpha/beta/rc tags
 release = '2.2.0'
 
-sys.path.insert(0, os.path.abspath("./"))
+# sys.path.insert(0, os.path.abspath("./"))
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'irsim')))
+
 # sys.path.insert(0, os.path.abspath("../"))
 # sys.path.insert(0, os.path.abspath("../.."))
 # sys.path.insert(0, os.path.abspath('../../'))
