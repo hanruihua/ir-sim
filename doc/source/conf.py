723610938669b7f925aab9164d70e2e71f6612fe
Han <hanrh@connect.hku.hk>
2024-08-31 02:54:51 +0800

    read the doc path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,6 +16,8 @@
 import os
 import sys
 
+
+
 # -- Project information -----------------------------------------------------
 
 project = 'IR-SIM'
@@ -32,8 +34,9 @@ release = '2.2.0'
 # sys.path.insert(0, os.path.abspath("./"))
 # sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'irsim')))
 
+root_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+sys.path.insert(0, root_path)
 
-# sys.path.insert(0, os.path.abspath("../"))
 # sys.path.insert(0, os.path.abspath("../.."))
 # sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath(".."))
