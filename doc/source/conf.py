b93d603c96d3de7ab2650bcbbd1a1b40a6948aeb
Han <744475118@qq.com>
2024-08-31 11:22:58 +0800

    read doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,8 @@ release = '2.2.0'
 # sys.path.insert(0, os.path.dirname(__file__))
 
 # sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath('../../../ir_sim/'))
+print(os.path.abspath('./'))
+sys.path.insert(0, os.path.abspath('./'))
 # sys.path.insert(0, os.path.abspath('../src'))
 # sys.path.append(os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath(".."))
