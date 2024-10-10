88ff423ccadd6c8079a6e355f375338d6a2679ae
Han <hanrh@connect.hku.hk>
2024-10-10 22:35:19 +0800

    Add lidar noise

--- irsim/usage/05lidar_world/lidar_world.py
+++ irsim/usage/05lidar_world/lidar_world.py
@@ -1,15 +1,13 @@
 import irsim
 
-env = irsim.make('lidar_world.yaml')
-# env = irsim.make('lidar_world_noise.yaml')
+# env = irsim.make('lidar_world.yaml')
+env = irsim.make('lidar_world_noise.yaml')
 
 for i in range(300):
 
     env.step()
     env.render(0.05)
     
-    print(env.get_lidar_scan())
-
     if env.done():
         break
 
