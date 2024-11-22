8cd41f4832503b81195eb9471ef0bb57864683dd
Han <744475118@qq.com>
2024-11-22 21:09:15 +0800

    Add util of random points

--- irsim/util/util.py
+++ irsim/util/util.py
@@ -606,5 +606,15 @@ def distance(point1, point2):
     """
     return sqrt((point1[0, 0] - point2[0, 0]) ** 2 + (point1[1, 0] - point2[1, 0]) ** 2)
 
+def random_point_range(range_low=[0, 0, -pi], range_high=[10, 10, pi]):
+
+    if isinstance(range_low, list):
+        range_low = np.c_[range_low]
+
+    if isinstance(range_low, list):
+        range_high = np.c_[range_high] 
+
+    return np.random.uniform(range_low, range_high)
+
 
 
