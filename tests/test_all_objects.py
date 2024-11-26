1d271b1864b10a603be226bbd51cf23292465a89
Han <744475118@qq.com>
2024-11-26 16:25:09 +0800

    Update test_all_objects.py

--- tests/test_all_objects.py
+++ tests/test_all_objects.py
@@ -2,11 +2,14 @@ import irsim
 
 def test_all_objects():
 
-    env = irsim.make('test_all_objects.yaml', display=False)
+    env = irsim.make(display=False)
 
-    for i in range(500):
+    for i in range(100):
 
         env.step()
         env.render(0.05)
 
     env.end()
+
+if __name__ == "__main__":
+    test_all_objects()
