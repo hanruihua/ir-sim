aeb586eba5db1985cf10981453f851aae002704d
Han <hanrh@connect.hku.hk>
2024-11-30 18:56:39 +0800

    reorganize lib

--- irsim/lib/__init__.py
+++ irsim/lib/__init__.py
@@ -1,4 +1,4 @@
-from irsim.lib.kinematics import (
+from irsim.lib.algorithm.kinematics import (
     differential_kinematics,
     ackermann_kinematics,
     omni_kinematics,
@@ -13,6 +13,7 @@ kinematics_factory = {
 from irsim.lib.behavior.behavior_registry import register_behavior
 from irsim.lib.behavior.behavior import Behavior
 
-from irsim.lib.generation import random_generate_polygon, generate_polygon
-
+from irsim.lib.algorithm.generation import random_generate_polygon, generate_polygon
 from irsim.lib.algorithm.rvo import reciprocal_vel_obs
+
+from irsim.lib.handler.kinematics_handler import kinematics_handler
