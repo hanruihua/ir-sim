a05dbaf41119a4a39dd520f24e03af46af70e27f
Han <hanrh@connect.hku.hk>
2024-12-04 23:08:26 +0800

    delete comment function

--- irsim/world/robots/robot_acker.py
+++ irsim/world/robots/robot_acker.py
@@ -27,21 +27,3 @@ class RobotAcker(ObjectBase):
             state_dim >= 4
         ), "for ackermann robot, the state dimension should be greater than 4"
 
-    # def plot_goal(
-    #     self, ax, goal_color="r", buffer_length=0.0, buffer_width=0.1, **kwargs
-    # ):
-
-    #     goal_x = self._goal[0, 0]
-    #     goal_y = self._goal[1, 0]
-    #     theta = self._goal[2, 0]
-
-    #     l = buffer_length + self.length
-    #     w = buffer_width + self.width
-
-    #     arrow = mpl.patches.Arrow(
-    #         goal_x, goal_y, l * cos(theta), l * sin(theta), width=w, color=goal_color
-    #     )
-    #     arrow.set_zorder(3)
-    #     ax.add_patch(arrow)
-
-    #     self.plot_patch_list.append(arrow)
