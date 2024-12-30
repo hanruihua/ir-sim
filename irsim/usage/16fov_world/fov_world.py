c728170ef126b867d23602d53657af5a13ed0abc
Han <hanrh@connect.hku.hk>
2024-12-30 23:44:11 +0800

    set goal

--- irsim/usage/16fov_world/fov_world.py
+++ irsim/usage/16fov_world/fov_world.py
@@ -6,14 +6,15 @@ for i in range(3000):
 
     env.step()
     # env.robot.set_state([x, y, theta])       
-    
+    # env.robot.set_goal([10, 30, 0])
+
     for obs in env.obstacle_list:
         if obs.fov_detect_object(env.robot):
             print(f'The robot is in the FOV of the {obs.name}. The parameters of this obstacle are: state [x, y, theta]: {obs.state.flatten()}, velocity [linear, angular]: {obs.velocity.flatten()}, fov in radian: {obs.fov}.')
 
     env.render()
 
-    if env.done():
-        break
+    # if env.done():
+    #     break
 
 env.end()
