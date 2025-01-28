0ee35d8283f07e65072b117bded6483487390407
Han <hanrh@connect.hku.hk>
2025-01-28 22:41:21 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2024, Ruihua Han'
 author = 'Ruihua Han'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.1'
+release = '2.3.2'
 
 # print(os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('../../'))
