0c6c6aece1c645d88e03d428a4f350cb01a5cf8a
Han <hanrh@connect.hku.hk>
2025-06-08 02:24:09 +0800

    temp save for mutiple goals

--- irsim/usage/17gui_world/gui.py
+++ irsim/usage/17gui_world/gui.py
@@ -27,8 +27,6 @@ Keyboard control:
 
 Mouse control:
 
-Button:
-
 '''
 
 
@@ -40,8 +38,9 @@ for i in range(10000):
     env.render(0.05, show_goal=False, show_trajectory=True)
     
     if env.mouse_left_pos is not None:
-        goal = np.pad(env.mouse_left_pos, (0, 1), 'constant', constant_values=0)
-        env.robot.append_goal(goal)
+        mouse_pos = np.pad(env.mouse_left_pos, (0, 1), 'constant', constant_values=0)
+        env.robot.append_goal(mouse_pos)
+        # env.robot.set_goal(mouse_pos)
 
     if env.done():
         break
