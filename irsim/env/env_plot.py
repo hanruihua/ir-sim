69c910016044e137f885616da8fe12014420b15d
hanruihua <hanrh@connect.hku.hk>
2025-07-04 02:02:33 +0800

    Fix gif generation by imageio v3 (#81)

--- irsim/env/env_plot.py
+++ irsim/env/env_plot.py
@@ -1,21 +1,22 @@
-'''
+"""
 This file is the implementation of the environment plot to visualize the environment objects.
 
 Author: Ruihua Han
-'''
+"""
 
 import matplotlib.pyplot as plt
 import logging
 from irsim.global_param.path_param import path_manager as pm
 from irsim.global_param import world_param, env_param
 import os
-import imageio.v2 as imageio
+import imageio.v3 as imageio
 import shutil
 import glob
 from math import sin, cos
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from typing import Optional
+
 
 class EnvPlot:
     """
@@ -43,7 +44,7 @@ class EnvPlot:
     ) -> None:
         """
         Initialize the EnvPlot instance.
-        
+
         Sets up the matplotlib figure, configures plotting parameters,
         and initializes the plot with world data and objects.
         """
@@ -59,13 +60,17 @@ class EnvPlot:
             "dpi": 100,
             "bbox_inches": "tight",
         }
-        self.saved_figure_kwargs.update(saved_figure) 
+        self.saved_figure_kwargs.update(saved_figure)
 
         # Create matplotlib figure and axes
         self.fig, self.ax = plt.subplots(
-            figsize=(figure_pixels[0] / self.saved_figure_kwargs['dpi'], figure_pixels[1] / self.saved_figure_kwargs['dpi']), dpi=self.saved_figure_kwargs['dpi']
+            figsize=(
+                figure_pixels[0] / self.saved_figure_kwargs["dpi"],
+                figure_pixels[1] / self.saved_figure_kwargs["dpi"],
+            ),
+            dpi=self.saved_figure_kwargs["dpi"],
         )
-        
+
         # Initialize plot settings and appearance
         self.color_map = {
             "robot": "g",
@@ -74,7 +79,7 @@ class EnvPlot:
             "target": "pink",
         }
         self.color_map.update(kwargs.get("color_map", dict()))
-    
+
         # Configure save options
         self.saved_ani_kwargs = {}
 
@@ -82,7 +87,7 @@ class EnvPlot:
         self.dyna_line_list = []
         self.dyna_point_list = []
         self.dyna_quiver_list = []
-        
+
         # Initialize the plot with world data
         self.init_plot(world.grid_map, objects, **kwargs)
 
@@ -101,7 +106,7 @@ class EnvPlot:
             self.ax.set_box_aspect([1, 1, 1])
         else:
             self.ax.set_aspect("equal")
-        
+
         self.ax.set_xlim(self.x_range)
         self.ax.set_ylim(self.y_range)
 
@@ -117,8 +122,10 @@ class EnvPlot:
         if tight:
             self.fig.tight_layout()
 
+    def step(self, mode="dynamic", objects=[], **kwargs):
 
-    def step(self, mode='dynamic', objects=[], **kwargs):
+        if self.show_title:
+            self.update_title()
 
         if isinstance(self.ax, Axes3D):
             self.clear_components(mode, objects)
@@ -127,26 +134,26 @@ class EnvPlot:
             self.clear_components(mode)
             self.step_objects_plot(mode, objects, **kwargs)
 
+    def init_objects_plot(self, objects, **kwargs):
+
         if self.show_title:
             self.update_title()
 
-    def init_objects_plot(self, objects, **kwargs):
         [obj._init_plot(self.ax, **kwargs) for obj in objects]
-        self.step_objects_plot('all', objects, **kwargs)
+        self.step_objects_plot("all", objects, **kwargs)
 
-    def step_objects_plot(self, mode='dynamic', objects=[], **kwargs):
+    def step_objects_plot(self, mode="dynamic", objects=[], **kwargs):
         """
         Update the plot for the objects.
         """
-        if mode == 'dynamic':
+        if mode == "dynamic":
             [obj._step_plot(**kwargs) for obj in objects if not obj.static]
-        elif mode == 'static':
+        elif mode == "static":
             [obj._step_plot(**kwargs) for obj in objects if obj.static]
-        elif mode == 'all':
+        elif mode == "all":
             [obj._step_plot(**kwargs) for obj in objects]
         else:
             self.logger.error("Error: Invalid draw mode")
-
 
     def draw_components(self, mode="all", objects=[], **kwargs):
         """
@@ -189,7 +196,7 @@ class EnvPlot:
 
         elif mode == "all":
             [obj.plot_clear(all=True) for obj in objects]
-            
+
             [line.pop(0).remove() for line in self.dyna_line_list]
             [points.remove() for points in self.dyna_point_list]
             [quiver.remove() for quiver in self.dyna_quiver_list]
@@ -198,7 +205,6 @@ class EnvPlot:
             self.dyna_point_list = []
             self.dyna_quiver_list = []
 
-        
     def draw_grid_map(self, grid_map=None, **kwargs):
         """
         Draw the grid map on the plot.
@@ -304,8 +310,7 @@ class EnvPlot:
         if refresh:
             self.dyna_point_list.append(points_plot)
 
-
-    def draw_quiver(self, point, refresh=False, color='black', **kwargs):
+    def draw_quiver(self, point, refresh=False, color="black", **kwargs):
         """
         Draw a quiver plot on the plot.
 
@@ -320,17 +325,19 @@ class EnvPlot:
         ax_point = self.ax.scatter(point[0], point[1], color=color)
 
         ax_quiver = self.ax.quiver(
-            point[0], point[1],  # starting positions
-            point[2], point[3],  # vector components (direction)
+            point[0],
+            point[1],  # starting positions
+            point[2],
+            point[3],  # vector components (direction)
             color=color,
-            **kwargs
+            **kwargs,
         )
 
         if refresh:
             self.dyna_quiver_list.append(ax_quiver)
             self.dyna_point_list.append(ax_point)
 
-    def draw_quivers(self, points, refresh=False, color='black', **kwargs):
+    def draw_quivers(self, points, refresh=False, color="black", **kwargs):
         """
         Draw a series of quiver plot on the plot.
 
@@ -338,7 +345,7 @@ class EnvPlot:
             points (list or np.ndarray): List of points, each point as [x, y, u, v]. u, v are the components of the vector.
 
         """
-        
+
         for point in points:
             self.draw_quiver(point, refresh, color=color, **kwargs)
 
@@ -361,7 +368,10 @@ class EnvPlot:
         if self.title is not None:
             self.ax.set_title(self.title, pad=3)
         else:
-            self.ax.set_title(f"Simulation Time: {self.world.time:.2f}s, Status: {self.world.status}", pad=3)
+            self.ax.set_title(
+                f"Simulation Time: {self.world.time:.2f}s, Status: {self.world.status}",
+                pad=3,
+            )
 
     def save_figure(
         self,
@@ -403,7 +413,7 @@ class EnvPlot:
         self,
         ani_name="animation",
         suffix=".gif",
-        keep_len=30,
+        last_frame_duration=1,
         rm_fig_path=True,
         **kwargs,
     ):
@@ -412,17 +422,14 @@ class EnvPlot:
 
         Args:
             ani_name (str): Name of the animation. Default is 'animation'.
-            keep_len (int): Length of the last frame. Default is 30.
+            last_frame_duration (int): Duration of the last frame for the gif. Default is 1 second.
             suffix (str): Suffix of the animation file. Default is '.gif'.
             rm_fig_path (bool): Whether to remove the figure path after saving. Default is True.
             kwargs: Additional arguments for saving the animation.
-                See `format_gif <https://imageio.readthedocs.io/en/v2.8.0/format_gif-pil.html>`_ for details.
+                See `imageio.imwrite <https://imageio.readthedocs.io/en/stable/_autosummary/imageio.v3.imwrite.html#imageio.v3.imwrite>`_ for details.
         """
-        
-        if suffix == ".gif":
-            self.saved_ani_kwargs.update({"subrectangles": True, "loop": 0})
 
-        self.saved_ani_kwargs.update(kwargs)    
+        self.saved_ani_kwargs.update(kwargs)
 
         self.logger.info("Start to create animation")
 
@@ -433,23 +440,20 @@ class EnvPlot:
             os.makedirs(ap)
 
         images = list(glob.glob(fp + "/*.png"))
-
         images.sort()
-        image_list = []
-        for i, file_name in enumerate(images):
-            if i == 0:
-                continue
+        image_list = [imageio.imread(str(file_name)) for file_name in images]
 
-            image_list.append(imageio.imread(str(file_name)))
-            if i == len(images) - 1:
-                for j in range(keep_len):
-                    image_list.append(imageio.imread(str(file_name)))
+        if suffix == ".gif":
+            # default arguments for gif
+            durations = [100] * (len(image_list) - 1) + [last_frame_duration * 1000]
+            self.saved_ani_kwargs.update(
+                {"plugin": "pillow", "duration": durations, "loop": 0}
+            )
 
-        imageio.mimsave(
-            ap + "/" + ani_name + suffix, image_list, **self.saved_ani_kwargs
-        )
+        full_name = ap + "/" + ani_name + suffix
+        imageio.imwrite(full_name, image_list, **self.saved_ani_kwargs)
 
-        self.logger.info("Animation created successfully, saved in " + ap)
+        self.logger.info(f"{ani_name} created successfully, saved in {ap}")
 
         if rm_fig_path:
             shutil.rmtree(fp)
@@ -465,10 +469,11 @@ class EnvPlot:
         Close the plot.
         """
         plt.close()
-    
+
     @property
     def logger(self):
         return env_param.logger
+
 
 def linewidth_from_data_units(linewidth, axis, reference="y"):
     """
@@ -501,5 +506,3 @@ def linewidth_from_data_units(linewidth, axis, reference="y"):
     length *= 72
     # Scale linewidth to value range
     return linewidth * (length / value_range)
-
-
