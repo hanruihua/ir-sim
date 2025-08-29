7353d4291f1aeec3171367de424a12bf87af8620
hanruihua <hanrh@connect.hku.hk>
2025-08-30 02:23:16 +0800

    feat: add environment key esc; x; (#117)
    * feat: add environment key esc; x;
    
    * fix type hint 3.9
    
    * test: improve test cov

--- usage/09keyboard_control/keyboard_control.py
+++ usage/09keyboard_control/keyboard_control.py
@@ -1,8 +1,9 @@
 import irsim
 
-env = irsim.make("keyboard_control.yaml", save_ani=False, full=False)
+env = irsim.make("keyboard_control_mpl.yaml", save_ani=False, full=False)
+# env = irsim.make("keyboard_control_pynput.yaml", save_ani=False, full=False)
 
-for _i in range(300):
+for _i in range(1000):
     env.step()
     env.render(0.05)
 
