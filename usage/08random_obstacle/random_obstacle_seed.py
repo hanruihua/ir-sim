8c934283f0057a980e6faff54909bd32d113b7cb
hanruihua <hanrh@connect.hku.hk>
2025-10-18 00:58:21 +0800

    style: update random seed usage

--- usage/08random_obstacle/random_obstacle_seed.py
+++ usage/08random_obstacle/random_obstacle_seed.py
@@ -2,6 +2,13 @@ import irsim
 
 env = irsim.make("random_obstacle.yaml", save_ani=False, full=False, seed=2)
 
+"""
+or
+env = irsim.make("random_obstacle.yaml", save_ani=False, full=False)
+env.set_random_seed(2)
+env.reload()
+"""
+
 for _i in range(300):
     env.step()
     env.render(0.05)
